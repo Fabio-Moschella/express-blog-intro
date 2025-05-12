57d51d9ff0dfd096cd40f11ad2fcf84166dd67e7
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-12 16:30:32 +0200

    add-staticexpress

--- blog.js
+++ blog.js
@@ -3,4 +3,14 @@ const express = require("express");
 const appBlog = express();
 const port = 3000;
 
-appBlog.get("/");
+appBlog.use(express.static("pubblic"));
+
+appBlog.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+appBlog.get("/bacheca", (req, res) => {
+  res.json(posts);
+});
+appBlog.listen(port, () => {
+  console.log("il server è in ascolto sulla porta" + port);
+});
