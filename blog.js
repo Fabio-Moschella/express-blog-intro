574a8b92268a511e78d3786d557738f82c7927c0
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-12 15:58:56 +0200

    Primo Commit

---
+++ blog.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const appBlog = express();
+const port = 3000;
+const posts = [
+  {
+    title: "Napoli",
+    content: "league",
+    image: "",
+    tags: "",
+  },
+  {
+    title: "Napoli",
+    content: "league",
+    image: "",
+    tags: "",
+  },
+  {
+    title: "Napoli",
+    content: "league",
+    image: "",
+    tags: "",
+  },
+  {
+    title: "Napoli",
+    content: "league",
+    image: "",
+    tags: "",
+  },
+  {
+    title: "Napoli",
+    content: "league",
+    image: "",
+    tags: "",
+  },
+];
+
+console.log("Hello World");
+
+// ??
+// Listen (SINCRONA)
+// Rotte
+
+// ??
+// Configurazioni
+// Middleware
+// Rotta 1
+// Rotta 2
+// Listen (SINCRONA)
